0199034b693c617fa0315aa989253e6a91add9b7
Ethan Kwong <eonnkwong@atlassian.com>
2026-03-15 12:38:57 +1100

    Debug updates

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ from handlers import messageHandler, commandHandler, buttonHandler
 
 app = Flask(__name__)
 
-# Configure logging to output to the console (which shows up in Cloud Run logs)
-logging.basicConfig(level=logging.INFO)
-
 @app.route('/', methods=['POST'])
 def webhook():
-    logging.info("Application startup complete")
-    # 1. Get the JSON data from the request
+    print("Application startup complete")
     data = request.get_json(force=True)
-    
-    message = data.get('message', {})
-    text = message.get('text', '')
 
-    if text:
-        if text.startswith('/'):
-            return commandHandler.processMessage(data)
-    elif 'callback_query' in data:  # Button clicks
+    print((f"Incoming Update: {data}"))
+    if 'callback_query' in data:
         return buttonHandler.processMessage(data)
-    else:
-        return messageHandler.processMessage(data)
+
+    message = data.get('message')
+    text = message.get('text', '')
+    if message:
+        text = message.get('text')
+        
+        if text:
+            if text.startswith('/'):
+                return commandHandler.processMessage(data)
+            else:
+                return messageHandler.processMessage(data)
     
-    # 4. Fallback for non-text messages (photos, voice, etc.)
-    logging.info("Received a non-text update (e.g., photo or sticker)")
+    print("Received a non-text update (e.g., photo or sticker)")
     return "OK", 200
